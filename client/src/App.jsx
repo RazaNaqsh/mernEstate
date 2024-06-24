d03737aab9bad9fb30f7e6aa1b185e0ccccd447f
RazaNaqsh <owaisraza1704@gmail.com>
2024-06-24 20:24:58 +0530

    Structure setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
