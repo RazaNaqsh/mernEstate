c2d8fb855879b31b2c8dd6332f91de84a6525c1f
RazaNaqsh <owaisraza1704@gmail.com>
2024-06-25 15:33:35 +0530

    Format using prettier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Home from "./pages/Home"
-import SignUp from "./pages/SignUp"
-import About from "./pages/About"
-import SignIn from "./pages/SignIn"
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const SignIn = () => {
-  return (
-    <div>Signin</div>
-  )
-}
+  return <div>Signin</div>;
+};
 
-export default SignIn
+export default SignIn;
